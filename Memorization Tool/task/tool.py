6f81113fe1c9584c282ccab242aed29975806632
parseltongist <>
2022-03-09 13:02:29 +0100

    stage 3 string.Template implemented

--- Memorization Tool/task/tool.py
+++ Memorization Tool/task/tool.py
@@ -1,8 +1,10 @@
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from string import Template
 
 WRONG = "WRONG OPTION\n"
+error_msg_template = Template("$value is not an option")
 
 Base = declarative_base()
 
@@ -31,7 +33,6 @@ class MemorizationTool:
             session = Session()
             session.add(new_flashcard)
             session.commit()  # session will be terminated after comit command
-
         while True:
             _option = input("1. Add a new flashcard\n2. Exit\n")
             match _option:
@@ -40,10 +41,32 @@ class MemorizationTool:
                 case "2":
                     return self.menu()
                 case _:
-                    print(f"{_option} is not an option")
+                    print(error_msg_template.substitute(value=_option))
 
     @staticmethod
     def practice_flashcards():
+        def update_flashcard():
+            # updating code
+            _choice = None
+            while not _choice:
+                _choice = input('press "d" to delete the flashcard:\npress "e" to edit the flashcard:\n')
+                match _choice:
+                    case "d":
+                        session.delete(row)
+                        session.commit()
+                    case "e":
+                        new_question = input(f"current question: {row.question}\nplease write a new question:\n")
+                        new_answer = input(f"current answer: {row.answer}\nplease write a new answer:\n")
+                        if all([new_question and new_answer]):
+                            question_filter = query.filter(FlashCards.id == row.id)
+                            question_filter.update({"question": new_question,
+                                                    "answer": new_answer}
+                                                   )
+                    case _:
+                        _choice = None  # reseting to None and continue the loop
+                        print(error_msg_template.substitute(value=_choice))
+            session.commit()
+
         session = Session()
         query = session.query(FlashCards)
         result_list = query.all()
@@ -53,38 +76,14 @@ class MemorizationTool:
         else:
             for row in result_list:
                 print(f"Question: {row.question}")
-                print('press "y" to see the answer:\npress "n" to skip:\npress "u" to update:')
-                answer = input().lower()
+                answer = input('press "y" to see the answer:\npress "n" to skip:\npress "u" to update:').lower()
                 match answer:
                     case "y":
                         print(f"Answer: {row.answer}")
                     case "n":
                         pass
                     case "u":
-                        # updating code
-                        _choice = None
-                        while not _choice:
-                            _choice = input('press "d" to delete the flashcard:\npress "e" to edit the flashcard:\n')
-                            match _choice:
-                                case "d":
-                                    session.delete(row)
-                                    session.commit()
-                                case "e":
-                                    print(f"current question: {row.question}")
-                                    # try with ".update()" method?
-                                    new_question = input("please write a new question:\n")
-                                    if new_question:
-                                        print(f"current answer: {row.answer}")
-                                        new_answer = input("please write a new answer:\n")
-                                        if new_answer:
-                                            question_filter = query.filter(FlashCards.id == row.id)
-                                            question_filter.update({"question": new_question,
-                                                                    "answer": new_answer}
-                                                                   )
-                                        session.commit()
-                                case _:
-                                    _choice = None  # reseting to None and continue the loop
-                                    print(f"{_choice} is not an option")
+                        update_flashcard()
 
     def menu(self):
         options = "1. Add flashcards\n"\
@@ -101,7 +100,7 @@ class MemorizationTool:
                 case "3":
                     exit(print("Bye!"))
                 case _:
-                    print(f"{choice} is not an option")
+                    print(error_msg_template.substitute(value=choice))
 
 
 if __name__ == '__main__':
